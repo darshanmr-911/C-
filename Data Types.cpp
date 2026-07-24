13156aa3dc701ddd37420318cdbbb3fd3d8cd9e6
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-07-24 20:48:37 +0530

    Add files via upload

---
+++ Data Types.cpp
@@ -0,0 +1,7 @@
+//C++ thier data types
+// C++ has several built-in data types that can be used to declare variables. The most commonly used data types in C++ are:
+// int: used to store integer values (whole numbers).
+// float: used to store floating-point numbers (numbers with decimal points).
+// double: used to store double-precision floating-point numbers (numbers with decimal points).
+// char: used to store single characters.   
+// bool: used to store boolean values (true or false).
