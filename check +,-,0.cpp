083ac9c0396a4c14908ba5a0cbc73d5c720381cd
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-07-26 21:45:19 +0530

    Add files via upload

---
+++ check +,-,0.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter the number:"<<endl;
+    cin>>num;
+
+    if (num==0)
+        cout<<"Zero"<<endl;
+    else if (num>0)
+        cout<<num <<" is Positive"<<endl;
+    else
+        cout<<num<<" is Negative";
+}
