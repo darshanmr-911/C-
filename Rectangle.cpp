fa22551307c6ff62ff7986a1b6f917a4d307c1e4
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-07-26 21:44:32 +0530

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int lenth, width,area;
+    cout<<"Enter the value of lenth:"<<endl;
+    cin>>lenth;
+    cout<<"Enter the value of width:"<<endl;
+    cin>>width;
+    area = lenth * width;
+    cout<<"Area = "<<area<<endl;
+
+
+}
