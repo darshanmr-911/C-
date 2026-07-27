1e9540b9ef226a87addf688ce1ef9d4ec56c4e6a
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-07-27 21:15:06 +0530

    Add files via upload

---
+++ enumerate.cpp
@@ -0,0 +1,34 @@
+//enum enum-name{list of names};
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    enum color{red=50,green,blue,yellow};
+    enum color c;
+    c=yellow;
+    cout<<"Value of c is:"<<c<<endl;
+    cout<<"Value of red is:"<<red<<endl;
+    cout<<"Value of green is:"<<green<<endl;
+    cout<<"Value of blue is:"<<blue<<endl;
+    cout<<"Value of yellow is:"<<yellow<<endl;
+    
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    enum color{red=50,green,blue,yellow};
+    enum color c;
+    c=yellow;
+    cout<<"Value of c is:"<<c<<endl;
+    cout<<"Value of red is:"<<red<<endl;
+    cout<<"Value of green is:"<<green<<endl;
+    cout<<"Value of blue is:"<<blue<<endl;
+    cout<<"Value of yellow is:"<<yellow<<endl;
+    cout<<"Value of c is:"<<c<<endl;
+}
