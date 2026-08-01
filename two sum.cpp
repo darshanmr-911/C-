792462a0db5a06042eeb78718f1838fc006a7d9b
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-08-01 21:41:55 +0530

    Add files via upload

--- two sum.cpp
+++ two sum.cpp
@@ -1,21 +1,18 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int first_number, second_number, sum;
-
-    // Prompt user for input
-    cout << "Enter two integers: ";
-
-    // Store user inputs into variables
-    cin >> first_number >> second_number;
-
-    // Calculate total using the + operator
-    sum = first_number + second_number;
-
-    // Display the result
-    cout << first_number << " + " << second_number << " = " << sum << endl;
-
-    return 0;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n ; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+    
+};
